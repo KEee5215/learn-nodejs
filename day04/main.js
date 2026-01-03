18323583c01a5b1789ec5f00afeedbaa14a460d3
KEee5215 <1470867731@qq.com>
2026-01-03 22:10:58 +0800

    day04: server router

--- day04/main.js
+++ day04/main.js
@@ -2,11 +2,20 @@
 import { readFile } from "node:fs/promises";
 import http from "node:http";
 
-const htmlFile = await readFile("./data.json", "utf-8");
+const jsonFile = await readFile("./data.json", "utf-8");
+const htmlFile = await readFile("./index.html", "utf-8");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(htmlFile);
+  if (req.url === "/data.json") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(jsonFile);
+  } else if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlFile);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 Not Found</h1>");
+  }
 });
 
 const PORT = 3000;
